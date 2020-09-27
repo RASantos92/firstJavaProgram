f76b685dcbbe9604c19a328911467b7551768d6f
Robert Santos <rasbms81608@gmail.com>
2020-09-27 14:12:47 -0500

    inital commit

---
+++ MyJavaProgram.java
@@ -0,0 +1,7 @@
+public class MyJavaProgram {
+    public static void main (String[] args){
+        System.out.println("My name is Robert Santos");
+        System.out.println("I am 28 years old");
+        System.out.println("My hometown is Fort Worth, TX");
+    }
+}
